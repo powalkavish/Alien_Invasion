3fb745b94f627201078b4c86f817b989535308fd
powalkavish <124475762+powalkavish@users.noreply.github.com>
2024-01-29 09:52:39 +0530

    Updating changes
    Deleting the old file to upload the new ones correctly

--- game1/gamestats.py
+++
@@ -1,9 +0,0 @@
-class GameStats():
-    
-    def __init__(self,setting):
-        self.setting = setting
-        self.gameflag = True
-        self.resetstats()
-        
-    def resetstats(self):
-        self.livesleft = self.setting.lives
